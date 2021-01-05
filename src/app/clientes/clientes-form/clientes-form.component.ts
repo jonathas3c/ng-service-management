620a3181018fc7a0d2ca4fff64302715c904e6df
Jonathas Germano <jonathas.germano@hotmail.com>
2021-01-05 16:31:53 -0300

    2-way data binding complete, form submit to build

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -12,14 +12,13 @@ export class ClientesFormComponent implements OnInit {
 
   constructor() { 
     this.cliente = new Cliente();
-    this.cliente.nome = 'Jonathas';
   }
 
   ngOnInit(): void {
   }
 
   clicar() {
-    console.log('Cliquei!');
+    console.log(this.cliente);
   }
 
 }
